a9322d896d0a315a54476a667f2d2e3ae1241bcf
Rohan <rohannyati65@gmail.com>
2021-10-06 19:22:46 +0530

    4 codes a Day

--- 100 Days of Code/Day 11/4.py
+++ 100 Days of Code/Day 11/4.py
@@ -1,1 +1,12 @@
-#  (leetcode)
+# 268. Missing Number (leetcode)
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        a=0
+        if a not in nums:
+            return(a)
+        
+        for i in range(len(nums)):
+            a=a+1
+            if a not in nums:
+                return(a)
+        
