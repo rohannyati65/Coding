5c6de93e4bed2284eb97713fbd386d83780540cd
Rohan <rohannyati65@gmail.com>
2021-10-07 14:32:43 +0530

    4 codes a day

--- 100 Days of Code/Day 12/2.py
+++ 100 Days of Code/Day 12/2.py
@@ -1,1 +1,17 @@
-#  (leetcode)
+# 167. Two Sum II - Input array is sorted (leetcode)
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        if len(numbers) > 10000:
+            numbers.append(target)
+            numbers.sort()
+            a = numbers.index(target)
+            return [a - 1, a]
+
+        for i in range(len(numbers) - 1):
+            for j in numbers[i + 1 :]:
+                if numbers[i] + j == target:
+                    a = i + 1
+                    b = numbers.index(j) + 1
+                    if a == b:
+                        b = b + 1
+                    return [a, b]
--- 100 Days of Code/Day 12/3.py
+++ 100 Days of Code/Day 12/3.py
@@ -1,1 +1,8 @@
-#  (leetcode)
+# 283. Move Zeroes (leetcode)
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        if 0 in nums:
+            for i in range(len(nums)):
+                if nums[i] == 0:
+                    nums.remove(0)
+                    nums.append(0)
