e811ffbcf52fc1dc9868c5709bce55288397c177
Rohan <rohannyati65@gmail.com>
2021-10-07 16:41:47 +0530

    4 codes a day

--- 100 Days of Code/Day 13/2.py
+++ 100 Days of Code/Day 13/2.py
@@ -1,1 +1,7 @@
-#  (leetcode)
+# 46. Permutations (leetcode)
+import itertools
+
+
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        return list(map(list, itertools.permutations(nums)))
