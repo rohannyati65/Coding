2f8010ed433a26f3d411f97c0c92efbf45005b40
Rohan <rohannyati65@gmail.com>
2021-10-08 12:50:18 +0530

    4 codes a day

--- 100 Days of Code/Day 13/3.py
+++ 100 Days of Code/Day 13/3.py
@@ -1,1 +1,14 @@
-#  (leetcode)
+# 557. Reverse Words in a String III (leetcode)
+class Solution:
+    def reverseWords(self, s: str) -> str:
+        l = list(map(str, s.split()))
+        # print(l)
+        p = ""
+        x = " "
+        for i in range(len(l)):
+            a = l[i]
+            if i == (len(l) - 1):
+                p = p + a[::-1]
+            else:
+                p = p + a[::-1] + x
+        return p
--- 100 Days of Code/Day 13/4.py
+++ 100 Days of Code/Day 13/4.py
@@ -1,1 +1,8 @@
-#  (leetcode)
+# 344. Reverse String (leetcode)
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        s[:] = s[::-1]
+        # if len(s)==1:
+        #    return(s)
+        # else:
+        #    s=self.reverseString(s[1:])+list(s[0])
