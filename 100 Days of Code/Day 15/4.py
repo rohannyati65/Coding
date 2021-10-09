f20e066bd76eebc1756bc8cad89967a6adc52648
Rohan <rohannyati65@gmail.com>
2021-10-09 21:51:04 +0530

    Day 15

--- 100 Days of Code/Day 15/4.py
+++ 100 Days of Code/Day 15/4.py
@@ -1,1 +1,14 @@
-#  (leetcode)
+# 451. Sort Characters By Frequency (leetcode)
+class Solution:
+    def frequencySort(self, s: str) -> str:
+        l = [i for i in s]
+        d = {}
+        s = list(set(l))
+        for i in s:
+            d[i] = l.count(i)
+        r = sorted(d.items(), key=lambda x: (-x[1], x[0]))
+        a = ""
+        for i in range(len(r)):
+            a = a + (r[i][0] * (l.count(r[i][0])))
+
+        return a
