fc0f5a27213a58c2067070d2eab5914eb4c2f4a9
Rohan <rohannyati65@gmail.com>
2021-10-10 10:23:04 +0530

    Day 16

--- 100 Days of Code/Day 16/3.py
+++ 100 Days of Code/Day 16/3.py
@@ -1,1 +1,17 @@
-#  (leetcode)
+# Count Pairs (PrepBytes)
+import itertools
+n=int(input())
+a=list(map(int,input().split()))
+b=list(map(int,input().split()))
+c=list(map(int,input().split()))
+
+l=[i for i in range(n)]
+lst=list(map(list,itertools.combinations(l,2)))
+count=0
+for i in lst:
+  x=i[0]
+  y=i[1]
+  if(a[x]==b[c[y]]):
+    count+=1
+print(count)
+
--- 100 Days of Code/Day 16/4.py
+++ 100 Days of Code/Day 16/4.py
@@ -1,1 +1,10 @@
-#  (leetcode)
+# 201. Bitwise AND of Numbers Range (leetcode)
+class Solution:
+    def rangeBitwiseAnd(self, left: int, right: int) -> int:
+        if left * 2 <= right:
+            return 0
+
+        x = left
+        for i in range(left + 1, right + 1):
+            x = x & i
+        return x
