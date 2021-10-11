27c6bcb294b4c9388e3f266dcb8114c4df31a551
Rohan <rohannyati65@gmail.com>
2021-10-11 09:24:39 +0530

    Day 17

--- 100 Days of Code/Day 17/1.py
+++ 100 Days of Code/Day 17/1.py
@@ -1,1 +1,10 @@
-#  (leetcode)
+# 567. Permutation in String (leetcode)
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        ordered = ''.join(sorted(s1))
+        for index in range(0, len(s2) - len(s1) + 1):
+            s = s2[index:(index+len(s1))]
+            s = ''.join(sorted(s))
+            if s == ordered:
+                return True
+        return False
--- 100 Days of Code/Day 17/2.py
+++ 100 Days of Code/Day 17/2.py
@@ -1,1 +1,33 @@
-#  (leetcode)
+# 3. Longest Substring Without Repeating Characters (leetcode)
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        l = []
+        if s == "":
+            return 0
+        if s == " ":
+            return 1
+        if len(s) == 1:
+            return 1
+        else:
+            for i in range(len(s)):
+                l1 = []
+                x = str(s[i:])
+                for j in x:
+                    if j not in l1:
+                        l1.append(j)
+                    elif j in l1:
+                        break
+                l.append(len(l1))
+
+            r = str(s[::-1])
+            for i in range(len(r)):
+                l1 = []
+                x = str(r[i:])
+                for j in x:
+                    if j not in l1:
+                        l1.append(j)
+                    elif j in l1:
+                        break
+                l.append(len(l1))
+
+            return max(l)
