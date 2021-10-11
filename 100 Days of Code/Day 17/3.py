13c8094dad4b2df5c16872fc0fed256f91f6caed
Rohan <rohannyati65@gmail.com>
2021-10-11 11:47:29 +0530

    Day 17

--- 100 Days of Code/Day 17/3.py
+++ 100 Days of Code/Day 17/3.py
@@ -1,1 +1,21 @@
-#  (leetcode)
+class Graph:
+    def __init__(self, gdict=None):
+        if gdict is None:
+            gdict = {}
+        self.gdict = gdict
+
+    def addEdge(self, vertex, edge):
+        self.gdict[vertex].append(edge)
+
+
+customDict = {
+    "a": ["b", "c", "d"],
+    "b": ["a", "e"],
+    "c": ["a", "d"],
+    "d": ["a", "c"],
+    "e": ["b", "d"],
+}
+
+graph = Graph(customDict)
+graph.addEdge("d", "e")
+print(graph.gdict)
