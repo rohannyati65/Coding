c3ccf7ae70528b18ee7b6deb65917faadf6bce38
Rohan <rohannyati65@gmail.com>
2021-10-12 09:39:25 +0530

    Day 17

--- 100 Days of Code/Day 17/4.py
+++ 100 Days of Code/Day 17/4.py
@@ -1,1 +1,21 @@
-#  (leetcode)
+# 374. Guess Number Higher or Lower (leetcode)
+# The guess API is already defined for you.
+# @param num, your guess
+# @return -1 if my number is lower, 1 if my number is higher, otherwise return 0
+# def guess(num: int) -> int:
+
+
+class Solution:
+    def guessNumber(self, n: int) -> int:
+        first = 1
+        last = n
+        while first <= last:
+            mid = (first + last) // 2
+            x = guess(mid)
+            if x == 0:
+                return mid
+            elif x < 0:
+                last = mid - 1
+            else:
+                first = mid + 1
+        return -1
