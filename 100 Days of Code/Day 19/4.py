f292a199a86dac8029738e9eaad110168a8d985f
Rohan <rohannyati65@gmail.com>
2021-10-14 00:40:42 +0530

    Day 19

--- 100 Days of Code/Day 19/4.py
+++ 100 Days of Code/Day 19/4.py
@@ -1,1 +1,13 @@
-#  (leetcode)
+# 2022. Convert 1D Array Into 2D Array (leetcode)
+class Solution:
+    def construct2DArray(self, original: List[int], m: int, n: int) -> List[List[int]]:
+        if (len(original) > (n * m)) or (len(original) < (n * m)):
+            return []
+
+        l = []
+        a = 0
+        for i in range(m):
+            l1 = original[a : a + n]
+            a = a + n
+            l.append(l1)
+        return l
