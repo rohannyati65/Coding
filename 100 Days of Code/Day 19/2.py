f53b4d95b7f78c23e70103df7e0e58c3eafe1df6
Rohan <rohannyati65@gmail.com>
2021-10-13 23:55:25 +0530

    Day 19

--- 100 Days of Code/Day 19/2.py
+++ 100 Days of Code/Day 19/2.py
@@ -1,1 +1,23 @@
-#  (leetcode)
+# 2032. Two Out of Three (leetcode)
+class Solution:
+    def twoOutOfThree(
+        self, nums1: List[int], nums2: List[int], nums3: List[int]
+    ) -> List[int]:
+        l = []
+        l = l + nums1 + nums2 + nums3
+        s = list(set(l))
+        s1 = list(s)
+        for i in s:
+            c = 0
+            if i in nums1:
+                c += 1
+
+            if i in nums2:
+                c += 1
+
+            if i in nums3:
+                c += 1
+
+            if c < 2:
+                s1.remove(i)
+        return s1
