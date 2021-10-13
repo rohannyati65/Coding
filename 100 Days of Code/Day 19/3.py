b17746c340944282f0119ad91a98fa8c3f195c28
Rohan <rohannyati65@gmail.com>
2021-10-14 00:29:56 +0530

    Day 19

--- 100 Days of Code/Day 19/3.py
+++ 100 Days of Code/Day 19/3.py
@@ -1,1 +1,24 @@
-#  (leetcode)
+# 2027. Minimum Moves to Convert String (leetcode)
+class Solution:
+    def minimumMoves(self, s: str) -> int:
+        f = 0
+        l = len(s) - 1
+        c = 0
+        while f <= l:
+            if s[f] == "X":
+                c += 1
+                f = f + 3
+            else:
+                f = f + 1
+        return c
+
+        """
+        if 'X' in s:
+            a=len(s)
+            if(a%3==0):
+                return(a//3)
+            else:
+                return((a//3)+1)
+            
+        return(0)
+        """
