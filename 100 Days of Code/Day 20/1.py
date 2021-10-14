1213e7c8479ac54ffa8f27e6bfb04e0cbc41aa8f
Rohan <rohannyati65@gmail.com>
2021-10-14 12:01:10 +0530

    Day 20

--- 100 Days of Code/Day 20/1.py
+++ 100 Days of Code/Day 20/1.py
@@ -1,1 +1,23 @@
-#  (leetcode)
+# 206. Reverse Linked List (leetcode)
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverseList(self, head: ListNode) -> ListNode:
+        helper = None
+        while head is not None:
+            next = head.next
+            head.next = helper
+            helper = head
+            head = next
+        return helper
+
+        # pre=ListNode(#head.val)
+        # while head:
+        #    head=head.next
+        #    a=ListNode(head.val)
+        #    a.next=pre
+        #    pre=a
+        # return(pre)
--- 100 Days of Code/Day 20/2.py
+++ 100 Days of Code/Day 20/2.py
@@ -1,1 +1,29 @@
-#  (leetcode)
+# 21. Merge Two Sorted Lists (leetcode)
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
+        curr = ListNode(0)
+        ans = curr
+        while l1 and l2:
+            if l1.val < l2.val:
+                curr.next = l1
+                l1 = l1.next
+            else:
+                curr.next = l2
+                l2 = l2.next
+            curr = curr.next
+        while l1:
+            curr.next = l1
+            l1 = l1.next
+            curr = curr.next
+
+        while l2:
+            curr.next = l2
+            l2 = l2.next
+            curr = curr.next
+
+        return ans.next
