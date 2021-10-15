fcaf6950d33a2c7c0bcd4aa34611fe61fe2a2de1
Rohan <rohannyati65@gmail.com>
2021-10-15 09:37:41 +0530

    Day 20

--- 100 Days of Code/Day 20/3.py
+++ 100 Days of Code/Day 20/3.py
@@ -1,1 +1,9 @@
-#  (leetcode)
+# 77. Combinations (leetcode)
+import itertools
+class Solution:
+    def combine(self, n: int, k: int) -> List[List[int]]:
+        l=[i for i in range(1,n+1)]
+        l1=list(map(list,itertools.combinations(l,k)))
+        return(l1)
+    
+    
