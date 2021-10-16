49b1ee4f180d597ee4368c3716444e6946a91337
Rohan <rohannyati65@gmail.com>
2021-10-16 21:21:04 +0530

    Day 22

--- 100 Days of Code/Day 22/3.py
+++ 100 Days of Code/Day 22/3.py
@@ -1,1 +1,22 @@
-#  (leetcode)
+# HackerEarth (October Circuit's 21)
+t = int(input())
+while t != 0:
+    s = input()
+    lst = [i for i in s]
+    l = lst.count("L")
+    r = lst.count("R")
+    u = lst.count("U")
+    d = lst.count("D")
+    x = abs(l - r)
+    y = abs(u - d)
+    if x % 2 != 0:
+        a = (x // 2) + 1
+    if x % 2 == 0:
+        a = x // 2
+    if y % 2 != 0:
+        b = (y // 2) + 1
+    if y % 2 == 0:
+        b = y // 2
+
+    print(a + b)
+    t = t - 1
