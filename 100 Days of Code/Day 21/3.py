dc00a6a976c4d7dc34b3bf545412c9c8459e8c33
Rohan <rohannyati65@gmail.com>
2021-10-16 13:46:52 +0530

    Day 21

--- 100 Days of Code/Day 21/3.py
+++ 100 Days of Code/Day 21/3.py
@@ -1,1 +1,32 @@
-#  (leetcode)
+# 784. Letter Case Permutation (leetcode)
+class Solution:
+    def letterCasePermutation(self, s: str) -> List[str]:
+        l = [s]
+        for i in range(len(s)):
+            # print(s[i])
+            if (s[i].isalpha()) == True:
+                l1 = []
+                if s[i].islower() == True:
+                    for j in l:
+                        if i == 0:
+                            l1.append(str(s[i].upper() + j[i + 1 :]))
+
+                        elif i == (len(s) - 1):
+                            l1.append(str(j[:i] + s[i].upper()))
+
+                        else:
+                            l1.append(str(j[:i] + s[i].upper() + j[i + 1 :]))
+
+                if s[i].isupper() == True:
+                    for j in l:
+                        if i == 0:
+                            l1.append(str(s[i].lower() + j[i + 1 :]))
+
+                        elif i == (len(s) - 1):
+                            l1.append(str(j[:i] + s[i].lower()))
+
+                        else:
+                            l1.append(str(j[:i] + s[i].lower() + j[i + 1 :]))
+                l = l + l1
+                # print(l)
+        return l
