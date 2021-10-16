6f04cc9416bd3f49958d082bf0c96cdca7f657dc
Rohan <rohannyati65@gmail.com>
2021-10-16 19:32:40 +0530

    Day 21

--- 100 Days of Code/Day 21/4.py
+++ 100 Days of Code/Day 21/4.py
@@ -1,1 +1,22 @@
-#  (leetcode)
+# Round G 2021 (Dogs and Cats)
+t = int(input())
+while t != 0:
+    n, d, c, m = map(int, input().split())
+    s = input()
+    y = "YES"
+    for i in range(len(s)):
+        if s[i] == "D":
+            if d == 0:
+                y = "NO"
+                break
+            d = d - 1
+            c = c + m
+
+        else:
+            if c == 0:
+                if s[i:].find("D") == True:
+                    y = "NO"
+                break
+            c = c - 1
+    print(y)
+    t = t - 1
