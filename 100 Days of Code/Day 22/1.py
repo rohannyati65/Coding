18a4ace2469a457600746581c3422ff89f7f876c
Rohan <rohannyati65@gmail.com>
2021-10-16 20:48:51 +0530

    Day 22

--- 100 Days of Code/Day 22/1.py
+++ 100 Days of Code/Day 22/1.py
@@ -1,1 +1,9 @@
-#  (leetcode)
+# 5885. Minimum Number of Moves to Seat Everyone (leetcode)
+class Solution:
+    def minMovesToSeat(self, seats: List[int], students: List[int]) -> int:
+        seats.sort()
+        students.sort()
+        c=0
+        for i in range(len(students)):
+            c=c+abs(students[i]-seats[i])
+        return(c)
--- 100 Days of Code/Day 22/2.py
+++ 100 Days of Code/Day 22/2.py
@@ -1,1 +1,19 @@
-#  (leetcode)
+# 5886. Remove Colored Pieces if Both Neighbors are the Same Color (leetcode)
+class Solution:
+    def winnerOfGame(self, colors: str) -> bool:
+        if len(colors) < 3:
+            return False
+        a = 0
+        b = 0
+        for i in range(0, len(colors) - 2):
+            if colors[i] == "A" and colors[i + 1] == "A" and colors[i + 2] == "A":
+                a = a + 1
+
+            if colors[i] == "B" and colors[i + 1] == "B" and colors[i + 2] == "B":
+                b = b + 1
+        # print(a,b)
+        if b >= a:
+            return False
+
+        if a > b:
+            return True
