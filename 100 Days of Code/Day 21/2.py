68d288413ea71b1f0e59261752ddcb09e1bc5cc3
Rohan <rohannyati65@gmail.com>
2021-10-16 10:06:16 +0530

    Day 21

--- 100 Days of Code/Day 21/2.py
+++ 100 Days of Code/Day 21/2.py
@@ -1,1 +1,13 @@
-#  (leetcode)
+# Problem Code: LUCKYNUM
+t = int(input())
+while t != 0:
+    a, b, c = map(int, input().split())
+    x = 0
+    if a == 7 or b == 7 or c == 7:
+        x = 1
+    if x == 1:
+        print("YES")
+    if x == 0:
+        print("NO")
+
+    t = t - 1
