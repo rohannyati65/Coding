1234bfacd0c943f7959987bcd4ad2c66e6b53135
Rohan <rohannyati65@gmail.com>
2021-10-16 10:04:32 +0530

    Day 21

--- 100 Days of Code/Day 21/1.py
+++ 100 Days of Code/Day 21/1.py
@@ -1,1 +1,16 @@
-#  (leetcode)
+# Problem Code: TESTSERIES
+t = int(input())
+while t != 0:
+    l = list(map(int, input().split()))
+    I = l.count(1)
+    E = l.count(2)
+    if I > E:
+        print("INDIA")
+
+    elif E > I:
+        print("ENGLAND")
+
+    else:
+        print("DRAW")
+
+    t = t - 1
