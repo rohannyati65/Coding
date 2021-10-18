960c5b9041198c8001ed7785bedb6efd284b1097
Rohan <rohannyati65@gmail.com>
2021-10-18 20:33:52 +0530

    Day 23

--- 100 Days of Code/Day 23/3.py
+++ 100 Days of Code/Day 23/3.py
@@ -1,1 +1,6 @@
-#  (leetcode)
+# 136. Single Number (leetcode)
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        for i in nums:
+            if nums.count(i) == 1:
+                return i
