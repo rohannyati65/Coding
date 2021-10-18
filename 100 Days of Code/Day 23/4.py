730cf97f3c757fa3a0ce59629e6791dbb6b975c6
Rohan <rohannyati65@gmail.com>
2021-10-18 21:06:25 +0530

    Day 23

--- 100 Days of Code/Day 23/4.py
+++ 100 Days of Code/Day 23/4.py
@@ -1,1 +1,9 @@
-#  (leetcode)
+# 191. Number of 1 Bits (leetcode)
+class Solution:
+    def hammingWeight(self, n: int) -> int:
+        # print(n)
+        y = bin(n)[2:]
+        # print(y)
+        l = [str(i) for i in y]
+        # print(l)
+        return l.count("1")
