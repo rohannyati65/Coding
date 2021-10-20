ea48334e99b4b59e1b2170a86ec9624443f33adb
Rohan <rohannyati65@gmail.com>
2021-10-20 15:16:55 +0530

    Day 24

--- 100 Days of Code/Day 24/2.py
+++ 100 Days of Code/Day 24/2.py
@@ -1,1 +1,19 @@
-#  (leetcode)
+# 151. Reverse Words in a String (leetcode)
+class Solution:
+    def reverseWords(self, s: str) -> str:
+        l = []
+        a = ""
+        for i in range(len(s)):
+            if s[i] == " ":
+                if len(a) > 0:
+                    l.append(a)
+                a = ""
+                pass
+            else:
+                a = a + s[i]
+        if len(a) > 0:
+            l.append(a)
+        # print(l)
+        l = l[::-1]
+        # print(l)
+        return " ".join(l)
