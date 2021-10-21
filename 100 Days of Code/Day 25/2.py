844fff9cad15492dcfb2c72c84223d60c8f932b1
Rohan <rohannyati65@gmail.com>
2021-10-21 12:36:03 +0530

    Day 25

--- 100 Days of Code/Day 25/2.py
+++ 100 Days of Code/Day 25/2.py
@@ -1,1 +1,20 @@
-#  (leetcode)
+# Input queries (October Circuits '21)
+t = int(input())
+while t != 0:
+    l, r, k = map(int, input().split())
+    left = r - k + 1
+    right = r
+    if left == 1:
+        if right == 1:
+            print(1)
+        else:
+            print(0)
+    elif left == 0:
+        print(0)
+    else:
+        i = right - 1
+        while i >= left and right != 0:
+            right = right & i
+            i = right - 1
+        print(right)
+    t = t - 1
