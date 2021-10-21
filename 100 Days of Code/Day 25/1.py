d9757f053d1f3f71cc1364567ac4800e71075bd9
Rohan <rohannyati65@gmail.com>
2021-10-21 09:56:25 +0530

    Day 25

--- 100 Days of Code/Day 25/1.py
+++ 100 Days of Code/Day 25/1.py
@@ -1,6 +1,23 @@
 #  Contest Code:START16C
 Problem Code:DIRECTN
 
+"Correct"
+t=int(input())
+while(t!=0):
+      n=int(input())
+      s=input()
+      l=[i for i in s]
+      x='NO'
+      for i in range(len(l)-1):
+            y=l[i:i+2]
+            if((y.count('L')==2) or (y.count('R')==2)):
+                  x='YES'
+                  break
+            
+      print(x)
+      t=t-1
+
+"GIVES TLE"
 t=int(input())
 while(t!=0):
       n=int(input())
