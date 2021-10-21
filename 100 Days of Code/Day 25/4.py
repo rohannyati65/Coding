5881d6d8e134bdbbed6135ef391fb076ea4f6568
Rohan <rohannyati65@gmail.com>
2021-10-21 21:09:09 +0530

    Day 25

--- 100 Days of Code/Day 25/4.py
+++ 100 Days of Code/Day 25/4.py
@@ -1,1 +1,17 @@
-#  (leetcode)
+# Dance Moves
+t = int(input())
+while t != 0:
+    x, y = map(int, input().split())
+    z = abs(y - x)
+    if y > x:
+        if z % 2 == 0:
+            a = z // 2
+        else:
+            a = (z // 2) + 1
+
+        x = x + (a * 2)
+        b = abs(y - x)
+        print(a + b)
+    else:
+        print(z)
+    t = t - 1
