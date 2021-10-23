0fab7ba0946d6c947e5a6ecf180c868f0387032e
Rohan <rohannyati65@gmail.com>
2021-10-23 15:17:51 +0530

    Day 26

--- 100 Days of Code/Day 26/3.py
+++ 100 Days of Code/Day 26/3.py
@@ -1,1 +1,21 @@
-#  (leetcode)
+# 209. Minimum Size Subarray Sum (leetcode)
+class Solution:
+    def minSubArrayLen(self, target: int, nums: List[int]) -> int:
+        l=0
+        r=0
+        s=0
+        res=len(nums)+1
+        while(r<len(nums)):
+            s=s+nums[r]
+            while(s>=target):
+                res=min([res,r-l+1])
+                s=s-nums[l]
+                l=l+1
+            r=r+1
+        return res if res<=len(nums) else 0
+            
+            
+            
+            
+        
+        
