dc3d83e3cd4e74aa5da76a05dc42467dc3401564
Rohan <rohannyati65@gmail.com>
2021-10-24 13:34:26 +0530

    Day 26

--- 100 Days of Code/Day 26/4.py
+++ 100 Days of Code/Day 26/4.py
@@ -1,1 +1,10 @@
-#  (leetcode)
+# 441. Arranging Coins (leetcode)
+class Solution:
+    def arrangeCoins(self, n: int) -> int:
+        if n == 0:
+            return 0
+        a = 1
+        while n >= a:
+            n = n - a
+            a = a + 1
+        return a - 1
