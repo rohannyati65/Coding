8bddb88457baffb7d7b5aa9bbee0a00a62ac0182
Rohan <rohannyati65@gmail.com>
2021-10-25 10:23:43 +0530

    Day 27

--- 100 Days of Code/Day 27/1.py
+++ 100 Days of Code/Day 27/1.py
@@ -1,1 +1,34 @@
-#  (leetcode)
+# 315. Count of Smaller Numbers After Self (leetcode)
+# 1st Attempt (2 pointer approach , TLE)
+class Solution:
+    def countSmaller(self, nums: List[int]) -> List[int]:
+        if len(nums) < 2:
+            return [0]
+        l = []
+        left = 0
+        right = 1
+        count = 0
+        while left < right:
+            # print(left,right)
+            if right == len(nums) - 1:
+                if left == (len(nums) - 2):
+                    if nums[left] > nums[right]:
+                        l.append(1)
+                    else:
+                        l.append(0)
+                    break
+                else:
+                    if nums[left] > nums[right]:
+                        count += 1
+
+                    l.append(count)
+                    left = left + 1
+                    right = left + 1
+                    count = 0
+            else:
+                if nums[left] > nums[right]:
+                    count += 1
+                right = right + 1
+
+        l.append(0)
+        return l
--- 100 Days of Code/Day 27/2.py
+++ 100 Days of Code/Day 27/2.py
@@ -1,1 +1,12 @@
-#  (leetcode)
+# 1365. How Many Numbers Are Smaller Than the Current Number (leetcode)
+class Solution:
+    def smallerNumbersThanCurrent(self, nums: List[int]) -> List[int]:
+        if len(nums) < 2:
+            return [0]
+        l = []
+        lst = list(nums)
+        lst.sort()
+        for i in nums:
+            a = lst.index(i)
+            l.append(a)
+        return l
