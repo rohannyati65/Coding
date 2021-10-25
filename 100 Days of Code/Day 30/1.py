503683a332a08bfe87ce83a8b893c1d7cc5a7ee5
Rohan <rohannyati65@gmail.com>
2021-10-26 01:56:24 +0530

    Day 30

--- 100 Days of Code/Day 30/1.py
+++ 100 Days of Code/Day 30/1.py
@@ -1,1 +1,16 @@
-#  (leetcode)
+# 1752. Check if Array Is Sorted and Rotated (leetcode)
+class Solution:
+    def check(self, nums: List[int]) -> bool:
+        if len(nums) == 1:
+            return True
+
+        x = 0
+        for i in range(len(nums) - 1):
+            if nums[i] > nums[i + 1]:
+                x = i + 1
+
+        a = nums[x:] + nums[:x]
+        nums.sort()
+        if nums == a:
+            return True
+        return False
