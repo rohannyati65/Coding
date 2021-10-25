2534d3ecff9864ab06804118103c898655bea15f
Rohan <rohannyati65@gmail.com>
2021-10-25 23:06:44 +0530

    Day 28

--- 100 Days of Code/Day 28/2.py
+++ 100 Days of Code/Day 28/2.py
@@ -1,1 +1,10 @@
-#  (leetcode)
+# 1455. Check If a Word Occurs As a Prefix of Any Word in a Sentence (leetcode)
+class Solution:
+    def isPrefixOfWord(self, sentence: str, searchWord: str) -> int:
+        l = list(map(str, sentence.split()))
+        x = len(searchWord)
+        for i in range(len(l)):
+            if len(l[i]) >= x:
+                if l[i][:x] == searchWord:
+                    return i + 1
+        return -1
