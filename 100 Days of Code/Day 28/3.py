18ca2aedf089807aa65838324036a768fcb33371
Rohan <rohannyati65@gmail.com>
2021-10-25 23:23:54 +0530

    Day 28

--- 100 Days of Code/Day 28/3.py
+++ 100 Days of Code/Day 28/3.py
@@ -1,1 +1,9 @@
-#  (leetcode)
+# 2000. Reverse Prefix of Word (leetcode)
+class Solution:
+    def reversePrefix(self, word: str, ch: str) -> str:
+        for i in range(len(word)):
+            if word[i] == ch:
+                x = word[: i + 1]
+                word = x[::-1] + word[i + 1 :]
+                break
+        return word
