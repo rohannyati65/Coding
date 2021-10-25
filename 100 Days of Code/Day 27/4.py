5b8b35d70f803a8440af2a1e0ce6813c8c6ea16e
Rohan <rohannyati65@gmail.com>
2021-10-25 14:40:44 +0530

    Day 27

--- 100 Days of Code/Day 27/4.py
+++ 100 Days of Code/Day 27/4.py
@@ -15,7 +15,32 @@ while t != 0:
     q = int(input())
     while q != 0:
         a, k = map(int, input().split())
-        if k > len(lst):
+        if k > len(lst) - 1:
+            k = len(lst) - 1
+        x = lst[k][a - 1]
+        print(x)
+        q = q - 1
+    t = t - 1
+
+
+# Codeforces Round #751 (Div. 2), problem: (B) Divine Array, Accepted
+t = int(input())
+while t != 0:
+    n = int(input())
+    l = list(map(int, input().split()))
+    lst = [[0]]
+    lst.append(l)
+    while lst[-1] != lst[-2]:
+        l1 = []
+        for i in lst[-1]:
+            l1.append(lst[-1].count(i))
+        lst.append(l1)
+    lst.remove(lst[0])
+    # print(lst)
+    q = int(input())
+    while q != 0:
+        a, k = map(int, input().split())
+        if k > len(lst) - 1:
             k = len(lst) - 1
         x = lst[k][a - 1]
         print(x)
