1640eac26571a27e9642d98dc72f1c9f08e7e80d
Rohan <rohannyati65@gmail.com>
2021-10-26 01:30:12 +0530

    Day 29

--- 100 Days of Code/Day 29/4.py
+++ 100 Days of Code/Day 29/4.py
@@ -1,1 +1,14 @@
-#  (leetcode)
+# 1816. Truncate Sentence (leetcode)
+class Solution:
+    def truncateSentence(self, s: str, k: int) -> str:
+        l = list(map(str, s.split()))
+        l = l[:k]
+        a = ""
+        for i in range(len(l)):
+            if i == len(l) - 1:
+                a = a + l[i]
+
+            else:
+                a = a + l[i] + " "
+
+        return a
