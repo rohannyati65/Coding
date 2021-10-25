a5bd2a6141d19efdf979c037dda980f24ec9a6b8
Rohan <rohannyati65@gmail.com>
2021-10-25 23:23:15 +0530

    Day 28

--- 100 Days of Code/Day 28/4.py
+++ 100 Days of Code/Day 28/4.py
@@ -1,1 +1,12 @@
-#  (leetcode)
+# 1893. Check if All the Integers in a Range Are Covered (leetcode)
+class Solution:
+    def isCovered(self, ranges: List[List[int]], left: int, right: int) -> bool:
+        l = []
+        for i in ranges:
+            l1 = [i for i in range(i[0], i[1] + 1)]
+            l = l + l1
+
+        for i in range(left, right + 1):
+            if i not in l:
+                return False
+        return True
