7d455e937a03442ba66ce0ae0af75ef7c3bb4418
Rohan <rohannyati65@gmail.com>
2021-10-26 00:40:39 +0530

    Day 29

--- 100 Days of Code/Day 29/2.py
+++ 100 Days of Code/Day 29/2.py
@@ -1,1 +1,17 @@
-#  (leetcode)
+# 1800. Maximum Ascending Subarray Sum (leetcode)
+class Solution:
+    def maxAscendingSum(self, nums: List[int]) -> int:
+        if len(nums) < 2:
+            return nums[0]
+
+        s = 0
+        x = nums[0]
+        for i in range(1, len(nums)):
+            if nums[i - 1] < nums[i]:
+                x = x + nums[i]
+                s = max([s, x])
+            else:
+                s = max([s, x])
+                x = nums[i]
+
+        return s
