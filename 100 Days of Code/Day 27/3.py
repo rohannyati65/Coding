f919532fa3722a58db4c81b1cabfd616e4e10d49
Rohan <rohannyati65@gmail.com>
2021-10-25 14:31:55 +0530

    Day 27

--- 100 Days of Code/Day 27/3.py
+++ 100 Days of Code/Day 27/3.py
@@ -1,1 +1,10 @@
-#  (leetcode)
+# Codeforces Round #751 (Div. 2), problem: (A) Two Subsequences
+t=int(input())
+while(t!=0):
+    s=input()
+    l=[i for i in s]
+    l.sort()
+    a=l[0]
+    s=s.replace(a,'',1)
+    print(a,s)
+    t=t-1
