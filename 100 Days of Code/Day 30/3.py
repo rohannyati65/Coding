866592b5aacbb4c6acd809ea988b667851d9480f
Rohan <rohannyati65@gmail.com>
2021-10-26 11:57:25 +0530

    Day 30

--- 100 Days of Code/Day 30/3.py
+++ 100 Days of Code/Day 30/3.py
@@ -1,1 +1,14 @@
-#  (leetcode)
+# 1003. Check If Word Is Valid After Substitutions (leetcode)
+class Solution:
+    def isValid(self, s: str) -> bool:
+        if len(s) % 3 != 0:
+            return False
+
+        while len(s) != 0:
+            if "abc" in s:
+                s = s.replace("abc", "")
+
+            else:
+                return False
+
+        return True
