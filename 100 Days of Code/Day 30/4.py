cb093ab7f6c2e0c4f37235ae227ae00639f54614
Rohan <rohannyati65@gmail.com>
2021-10-26 15:44:04 +0530

    Day 30

--- 100 Days of Code/Day 30/4.py
+++ 100 Days of Code/Day 30/4.py
@@ -1,1 +1,17 @@
-#  (leetcode)
+# 1437. Check If All 1's Are at Least Length K Places Away (leetcode)
+class Solution:
+    def kLengthApart(self, nums: List[int], k: int) -> bool:
+        if 1 not in nums:
+            return True
+
+        x = nums.index(1)
+        for i in range(x + 1, len(nums)):
+            if nums[i] == 1:
+                # print(i,x)
+                y = i - x - 1
+                # print(y)
+                if y >= k:
+                    x = i
+                else:
+                    return False
+        return True
