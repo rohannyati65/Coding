acfb5999d4ede19031d20056cc16aa06d8a18e9b
Rohan <rohannyati65@gmail.com>
2021-10-26 20:53:18 +0530

    Day 32

--- 100 Days of Code/Day 32/1.py
+++ 100 Days of Code/Day 32/1.py
@@ -1,1 +1,7 @@
-#  (leetcode)
+# 628. Maximum Product of Three Numbers (leetcode)
+class Solution:
+    def maximumProduct(self, nums: List[int]) -> int:
+        nums.sort()
+        x = nums[-1] * nums[-2] * nums[-3]
+        y = nums[-1] * nums[0] * nums[1]
+        return max(x, y)
