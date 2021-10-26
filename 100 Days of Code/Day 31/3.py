68c265a6b7988226569d9cc12c46f3e3278ef3cb
Rohan <rohannyati65@gmail.com>
2021-10-26 20:08:37 +0530

    Day 31

--- 100 Days of Code/Day 31/3.py
+++ 100 Days of Code/Day 31/3.py
@@ -1,1 +1,13 @@
-#  (leetcode)
+# 326. Power of Three (leetcode)
+class Solution:
+    def isPowerOfThree(self, n: int) -> bool:
+        #if(n==1):
+        #    return(False)
+        
+        while(n>=1):
+            if(n==1):
+                return(True)
+            n=n/3
+        return(False)
+            
+            
--- 100 Days of Code/Day 31/4.py
+++ 100 Days of Code/Day 31/4.py
@@ -1,1 +1,8 @@
-#  (leetcode)
+# 342. Power of Four (leetcode)
+class Solution:
+    def isPowerOfFour(self, n: int) -> bool:
+        while n >= 1:
+            if n == 1:
+                return True
+            n = n / 4
+        return False
