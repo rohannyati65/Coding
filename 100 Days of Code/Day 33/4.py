eed66742d51f0802fb1f2b6fa58a8f85632934b3
Rohan <rohannyati65@gmail.com>
2021-10-27 22:22:56 +0530

    Day 33

--- 100 Days of Code/Day 33/4.py
+++ 100 Days of Code/Day 33/4.py
@@ -1,1 +1,13 @@
-#  (leetcode)
+#  Contest Code:LTIME101C
+# Problem Code:PROBCAT
+
+t = int(input())
+while t != 0:
+    x = int(input())
+    if x >= 1 and x < 100:
+        print("Easy")
+    if x >= 100 and x < 200:
+        print("Medium")
+    if x >= 200 and x <= 300:
+        print("Hard")
+    t = t - 1
--- 100 Days of Code/Day 34/1.py
+++ 100 Days of Code/Day 34/1.py
@@ -1,1 +1,18 @@
-#  (leetcode)
+#  Contest Code:LTIME101C
+# Problem Code:N1VALUES
+
+t=int(input())
+while(t!=0):
+      n=int(input()) 
+      if(n==1):
+            print(*[1,1])
+      else:
+            x=2**n
+            l=[i for i in range(1,n)]
+            l.append(n-1)
+            l.append(x-sum(l))
+            print(*l)
+      t=t-1
+
+
+
