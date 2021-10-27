42a59e28da945010d0b54e0aeaae97a63fdd8600
Rohan <rohannyati65@gmail.com>
2021-10-27 17:23:38 +0530

    Day 33

--- 100 Days of Code/Day 33/3.py
+++ 100 Days of Code/Day 33/3.py
@@ -1,1 +1,18 @@
-#  (leetcode)
+# 75. Sort Colors (leetcode)
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        l1=[]
+        l2=[]
+        l3=[]
+        for i in nums:
+            if(i==0):
+                l1.append(i)
+            if(i==1):
+                l2.append(i)
+            if(i==2):
+                l3.append(i)
+        nums[:]=l1+l2+l3
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        
