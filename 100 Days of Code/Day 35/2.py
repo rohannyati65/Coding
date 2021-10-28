88a093ee4af18329e62f89b7ba7ab2afd472a74d
Rohan <rohannyati65@gmail.com>
2021-10-29 00:15:47 +0530

    Day 35

--- 100 Days of Code/Day 35/2.py
+++ 100 Days of Code/Day 35/2.py
@@ -1,1 +1,23 @@
-#  (leetcode)
+# 1390. Four Divisors (leetcode)
+import math
+
+
+class Solution:
+    def sumFourDivisors(self, nums: List[int]) -> int:
+        s = 0
+        for i in nums:
+            x = 1 + i
+            y = 2
+            for j in range(2, math.floor(math.sqrt(i)) + 1):
+                if i % j == 0:
+                    y = y + 1
+                    x = x + j
+                    if j != (i // j):
+                        x = x + (i // j)
+                        y += 1
+                if y > 4:
+                    break
+            if y == 4:
+                s = s + x
+
+        return s
