05f6d5b54a4991d224ff132141a4ff7685fd9120
Rohan <rohannyati65@gmail.com>
2021-10-28 15:59:12 +0530

    Day 35

--- 100 Days of Code/Day 35/1.py
+++ 100 Days of Code/Day 35/1.py
@@ -1,1 +1,8 @@
-#  (leetcode)
+# 231. Power of Two (leetcode)
+class Solution:
+    def isPowerOfTwo(self, n: int) -> bool:
+        while(n>=1):
+            if(n==1):
+                return(True)
+            n=n/2
+        return(False)
