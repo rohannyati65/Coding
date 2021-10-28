8bd723bb3f41926db5393ee356c77faababfd068
Rohan <rohannyati65@gmail.com>
2021-10-28 10:29:09 +0530

    Day 34

--- 100 Days of Code/Day 34/2.py
+++ 100 Days of Code/Day 34/2.py
@@ -1,1 +1,20 @@
-#  (leetcode)
+# 1859. Sorting the Sentence (leetcode)
+class Solution:
+    def sortSentence(self, s: str) -> str:
+        l = list(map(str, s.split()))
+        l1 = []
+        for i in range(len(l)):
+            x = l[i][-1]
+            l1.append(int(x))
+            l[i] = l[i][:-1]
+        # print(l,l1)
+        y = len(l1)
+        a = ""
+        for i in range(1, y + 1):
+            x = l1.index(i)
+            if i == y:
+                a = a + l[x]
+            else:
+                a = a + l[x] + " "
+
+        return a
