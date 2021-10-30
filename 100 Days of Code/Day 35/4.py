ffc5f2f059f281bd3bc0d81ea8abdf2b4c7a0982
Rohan <rohannyati65@gmail.com>
2021-10-30 21:28:36 +0530

    Day 35

--- 100 Days of Code/Day 35/4.py
+++ 100 Days of Code/Day 35/4.py
@@ -1,1 +1,11 @@
-#  (leetcode)
+# 5898. Kth Distinct String in an Array (leetcode)
+class Solution:
+    def kthDistinct(self, arr: List[str], k: int) -> str:
+        a = 0
+        for i in arr:
+            if arr.count(i) == 1:
+                a += 1
+                # print(i,a)
+                if a == k:
+                    return i
+        return ""
--- 100 Days of Code/Day 36/1.py
+++ 100 Days of Code/Day 36/1.py
@@ -1,1 +1,27 @@
-#  (leetcode)
+# 5900. Plates Between Candles (leetcode)
+
+# 1st Attempt (TLE)
+class Solution:
+    def platesBetweenCandles(self, s: str, queries: List[List[int]]) -> List[int]:
+        l1 = []
+        for i in queries:
+            l = i[0]
+            r = i[1]
+            x = s[l : r + 1]
+            if "*" not in x:
+                c = 0
+
+            elif "|" not in x:
+                c = 0
+
+            else:
+                y = x.count("|")
+                a = x.index("|")
+                z = x[::-1]
+                b = z.index("|")
+                c = len(x)
+                c = c - b
+                c = c - a
+                c = c - y
+            l1.append(c)
+        return l1
