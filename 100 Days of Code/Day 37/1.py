96e5a4af40f4b62a6c4e1af0bdbd7290d36d0d04
Rohan <rohannyati65@gmail.com>
2021-10-31 15:38:39 +0530

    Day 37

--- 100 Days of Code/Day 37/1.py
+++ 100 Days of Code/Day 37/1.py
@@ -1,1 +1,84 @@
-#  (leetcode)
+# Contest Code:SNCK1B21 Problem Code:LARGESTLADDU
+
+t=int(input())
+while(t!=0):
+    n=int(input())
+    l=list(map(int,input().split()))
+    x="YES"
+    n=2**n
+    while(n!=1):
+        l.sort()    
+        l1=[]
+        a=0
+        for i in range(n//2):
+            if(abs(l[a]-l[a+1])<=1):
+                l1.append(l[a]+l[a+1])
+                a=a+2
+            else:
+                x="NO"
+                break
+        
+        if(x=="NO"):
+            break
+        
+        l=l1
+        n=n//2
+    
+    print(x)
+    t=t-1
+
+
+
+# cpp code (Correct)
+
+#include <bits/stdc++.h>
+using namespace std;
+#define tc ll t sc cin >> t sc while (t--)
+#define ff first
+#define vp vector<pair<ll,ll>>
+#define sc ;
+#define pb push_back
+
+#define ll long long int
+#define MAX(a, b) a = max(a, b)
+#define MIN(a, b) a = min(a, b)
+#define INF 1001001001
+const long double pi = 3.141592653;
+
+typedef unsigned int ui;
+typedef unsigned long long int ul;
+void
+_print (ul t)
+{
+  cerr << t;
+}
+
+int
+main ()
+{
+  tc
+  {
+    int n;
+    cin >> n;
+    vector < int >v;
+    ul size = pow (2, n);
+    for (int i = 0; i < size; i++)
+      {
+	int x;
+	cin >> x;
+	v.pb (x);
+      }
+    int minn = *min_element (v.begin (), v.end ());
+    int maxx = *max_element (v.begin (), v.end ());
+    if (maxx - minn <= 1)
+      {
+	cout << "YES" << endl;
+      }
+    else
+      {
+	cout << "NO" << endl;
+      }
+  }
+  return 0;
+
+}
--- 100 Days of Code/Day 37/2.py
+++ 100 Days of Code/Day 37/2.py
@@ -1,1 +1,54 @@
-#  (leetcode)
+# Contest Code:SNCK1B21 Problem Code:HLPNISHANT
+
+t=int(input())
+while(t!=0):
+    k=int(input())
+    l=[k]
+    k=k-1
+    while(k>=2):
+        if(len(l)==1):
+            x=k*2-l[0]-1
+        else:
+            x=k*2-l[1]-1
+            
+        if(x>1 and x<k):
+            if k not in l:
+                l.insert(0,k)
+            
+            l.insert(0,x)
+            k=x
+        
+        else:
+            k=k-1
+            
+    if(len(l)<2):
+        print(4)
+    else:
+        print(len(l)*2)
+    t=t-1
+
+
+# cpp code (Correct)
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    long long t = 1;
+    cin >> t;
+    while (t--)
+    {
+        long double m;
+        cin >> m;
+        long double n = m;
+        m = (long double)sqrt(2 * m + 0.25) - 0.5;
+        m = floor(m);
+        long double k = (m + 1) * m;
+        k /= 2;
+        if (k >= n)
+            m--;
+        cout << (2 * m + 2) << endl;
+    }
+    return 0;
+}
