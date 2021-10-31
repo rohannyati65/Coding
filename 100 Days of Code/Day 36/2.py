8669540e04c5773e3b0e7849bc6012517c90657b
Rohan <rohannyati65@gmail.com>
2021-10-31 08:47:54 +0530

    Day 36

--- 100 Days of Code/Day 36/2.py
+++ 100 Days of Code/Day 36/2.py
@@ -1,1 +1,7 @@
-#  (leetcode)
+# 5914. Smallest Index With Equal Value (leetcode)
+class Solution:
+    def smallestEqual(self, nums: List[int]) -> int:
+        for i in range(len(nums)):
+            if i % 10 == nums[i]:
+                return i
+        return -1
