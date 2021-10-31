32fcb1f66ec35bf8586402363f081896e3d6d6be
Rohan <rohannyati65@gmail.com>
2021-10-31 20:27:48 +0530

    Day 37

--- 100 Days of Code/Day 37/4.py
+++ 100 Days of Code/Day 37/4.py
@@ -1,1 +1,12 @@
-#  (leetcode)
+#  Contest Code:CCOE2021
+# Problem Code:BADWORD
+
+t = int(input())
+while t != 0:
+    s = input()
+    x = "hello"
+    if x in s:
+        print("YES")
+    else:
+        print("NO")
+    t = t - 1
