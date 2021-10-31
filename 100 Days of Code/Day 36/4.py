059ee524e65691ef3979c7b9e0058bb1795b9791
Rohan <rohannyati65@gmail.com>
2021-10-31 12:33:28 +0530

    Day 36

--- 100 Days of Code/Day 36/4.py
+++ 100 Days of Code/Day 36/4.py
@@ -1,1 +1,37 @@
-#  (leetcode)
+# Contest Code:SNCK1B21 Problem Code:QPLACE
+t = int(input())
+while t != 0:
+    N = int(input())
+    z = N
+    q = N - 2
+    l = []
+    if N > 3:
+        x = N - 3
+        for i in range(x):
+            l1 = "." * (N - i - 1)
+            l1 = l1 + "Q"
+            l2 = "." * (i)
+            l1 = l1 + l2
+            l.append(l1)
+        N = 3
+
+    if N == 3:
+        l1 = "." * z
+        l.append(l1)
+        N = N - 1
+
+    if N == 2:
+        l1 = ".Q"
+        l2 = "." * (z - 2)
+        l1 = l1 + l2
+        l.append(l1)
+        N = N - 1
+
+    if N == 1:
+        l1 = "." * z
+        l.append(l1)
+        N = N - 1
+
+    for i in l:
+        print(i)
+    t = t - 1
