36166e633822a959dc01afce4b94d0338ff04681
Rohan <rohannyati65@gmail.com>
2021-10-31 17:22:27 +0530

    Day 37

--- 100 Days of Code/Day 37/3.py
+++ 100 Days of Code/Day 37/3.py
@@ -1,1 +1,10 @@
-#  (leetcode)
+# Contest Code:IICP2021 Problem Code:KILLFEED
+
+t = int(input())
+while t != 0:
+    n = int(input())
+    l = list(map(int, input().split()))
+    y = sum(l)
+    z = max(l)
+    print(y - z)
+    t = t - 1
