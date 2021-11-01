1bd0c59b8fd0087f11a5a3faa44216978f44cee2
Rohan <rohannyati65@gmail.com>
2021-11-01 15:27:14 +0530

    Day 38

--- 100 Days of Code/Day 38/1.py
+++ 100 Days of Code/Day 38/1.py
@@ -1,1 +1,33 @@
-#  (leetcode)
+# 389. Find the Difference (leetcode)
+class Solution:
+    def findTheDifference(self, s: str, t: str) -> str:
+        if len(s) == 0:
+            return t
+
+        s = [i for i in s]
+        t = [i for i in t]
+
+        l1 = list(set(s))
+        l2 = list(set(t))
+
+        l1.sort()
+        l2.sort()
+        x = ""
+
+        for i in l2:
+            a = t.count(i)
+            b = s.count(i)
+            if a != b:
+                c = a - b
+                # print(i)
+                x = x + i * c
+                break
+        return x
+
+        """
+        x=""
+        for i in range(len(t)):
+            if(i>=len(s)):
+                x=x+t[i]
+            else:
+        """
