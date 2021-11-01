5485daa42c9e28793b91c3b6a451fb1449fefff0
Rohan <rohannyati65@gmail.com>
2021-11-01 20:27:03 +0530

    Day 38

--- 100 Days of Code/Day 38/4.py
+++ 100 Days of Code/Day 38/4.py
@@ -1,1 +1,13 @@
-#  (leetcode)
+# 409. Longest Palindrome (leetcode)
+class Solution:
+    def longestPalindrome(self, s: str) -> int:
+        ss = set()
+        for letter in s:
+            if letter not in ss:
+                ss.add(letter)
+            else:
+                ss.remove(letter)
+        if len(ss) != 0:
+            return len(s) - len(ss) + 1
+        else:
+            return len(s)
