edce01e393454568b77cd6f950a6b02107489207
Rohan <rohannyati65@gmail.com>
2021-11-01 15:49:47 +0530

    Day 38

--- 100 Days of Code/Day 38/2.py
+++ 100 Days of Code/Day 38/2.py
@@ -1,1 +1,20 @@
-#  (leetcode)
+# 345. Reverse Vowels of a String (leetcode)
+class Solution:
+    def reverseVowels(self, s: str) -> str:
+        x = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"]
+        s = [i for i in s]
+        s1 = s[::-1]
+        l = []
+        for i in s1:
+            if i in x:
+                l.append(i)
+
+        a = 0
+        y = ""
+        for i in range(len(s)):
+            if s[i] in x:
+                y = y + l[a]
+                a = a + 1
+            else:
+                y = y + s[i]
+        return y
