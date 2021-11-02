06e58626851718b19ec90df774fb8a3982b4efdd
Rohan <rohannyati65@gmail.com>
2021-11-02 17:25:11 +0530

    Day 39

--- 100 Days of Code/Day 39/4.py
+++ 100 Days of Code/Day 39/4.py
@@ -1,1 +1,23 @@
-#  (leetcode)
+# HackerEarth (Hiring Challenge)
+def solve (N, X, Y, A, B):
+    s=0
+    for i in range(len(A)):
+        a=A[i]
+        for j in range(len(B)):
+            b=B[j]
+            if(((a^b)&X)==((a^b)&Y)):
+                s=s+1
+    return(s)
+    # Write your code here
+    #pass
+
+T = int(input())
+for _ in range(T):
+    N = int(input())
+    X = int(input())
+    Y = int(input())
+    A = list(map(int, input().split()))
+    B = list(map(int, input().split()))
+
+    out_ = solve(N, X, Y, A, B)
+    print (out_)
--- 100 Days of Code/Day 40/1.py
+++ 100 Days of Code/Day 40/1.py
@@ -1,1 +1,34 @@
-#  (leetcode)
+# HackerEarth (Hiring Challenge)
+def solve(N, A, Q, query):
+    l1 = []
+    for j in query:
+        l = list(A)
+        left = j[1]
+        right = j[2]
+        if j[0] == 1:
+            for i in range(left, right):
+                l[i] = 1 - max(l[i], l[i - 1])
+
+            x = l[right - 1]
+
+        if j[0] == 2:
+            for i in range(left, right):
+                l[i] = 1 - min(l[i], l[i - 1])
+
+            x = l[right - 1]
+        l1.append(x)
+    return l1
+
+    # Write your code here
+    # pass
+
+
+T = int(input())
+for _ in range(T):
+    N = int(input())
+    A = list(map(int, input().split()))
+    Q = int(input())
+    query = [list(map(int, input().split())) for i in range(Q)]
+
+    out_ = solve(N, A, Q, query)
+    print(" ".join(map(str, out_)))
