c5d8dca067f599769ac007490f69f2beee75c404
Rohan <rohannyati65@gmail.com>
2021-11-02 14:48:13 +0530

    Day 39

--- 100 Days of Code/Day 39/2.py
+++ 100 Days of Code/Day 39/2.py
@@ -1,1 +1,5 @@
-#  (leetcode)
+# 434. Number of Segments in a String (leetcode)
+class Solution:
+    def countSegments(self, s: str) -> int:
+        l = list(map(str, s.split()))
+        return len(l)
