f3f5959b439f3c5fc1d7fc2c94fdb90018d18cad
Rohan <rohannyati65@gmail.com>
2021-11-02 15:28:28 +0530

    Day 39

--- 100 Days of Code/Day 39/3.py
+++ 100 Days of Code/Day 39/3.py
@@ -1,1 +1,13 @@
-#  (leetcode)
+# 459. Repeated Substring Pattern (leetcode)
+class Solution:
+    def repeatedSubstringPattern(self, s: str) -> bool:
+        i = 1
+        while i != (len(s) // 2) + 1:
+            x = s[:i]
+            i = i + 1
+            y = s.count(x)
+            # print(x,y)
+            if y * len(x) == len(s):
+                return True
+
+        return False
