b38d37349393e8b889b8fe0be00017d7909c853b
Rohan <rohannyati65@gmail.com>
2021-11-03 15:49:31 +0530

    Day 40

--- 100 Days of Code/Day 40/4.py
+++ 100 Days of Code/Day 40/4.py
@@ -1,1 +1,16 @@
-#  (leetcode)
+# 521. Longest Uncommon Subsequence I (leetcode)
+class Solution:
+    def findLUSlength(self, a: str, b: str) -> int:
+        if len(b) > len(a):
+            a, b = b, a
+
+        if a != b:
+            return len(a)
+
+        for i in b:
+            if i in a:
+                a = a.replace(i, "", 1)
+        if len(a) == 0:
+            return -1
+
+        return len(a)
