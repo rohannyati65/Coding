a8d6db099dd4f6ae788d1d052f84528b8b6966a6
Rohan <rohannyati65@gmail.com>
2021-11-03 15:40:32 +0530

    Day 40

--- 100 Days of Code/Day 40/3.py
+++ 100 Days of Code/Day 40/3.py
@@ -1,1 +1,21 @@
-#  (leetcode)
+# 520. Detect Capital (leetcode)
+class Solution:
+    def detectCapitalUse(self, word: str) -> bool:
+        if len(word) == 1:
+            return True
+
+        if word[0].isupper() == True:
+            if word[1:].islower() == True:
+                return True
+
+            elif word[1:].isupper() == True:
+                return True
+
+            else:
+                return False
+
+        if word[0].islower() == True:
+            if word[1:].islower() == True:
+                return True
+
+            return False
