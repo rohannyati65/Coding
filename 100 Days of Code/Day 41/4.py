2004b81e0a28edc25711d6281a99f7056ca21d79
Rohan <rohannyati65@gmail.com>
2021-11-05 10:55:01 +0530

    Day 41

--- 100 Days of Code/Day 41/4.py
+++ 100 Days of Code/Day 41/4.py
@@ -1,1 +1,18 @@
-#  (leetcode)
+# 748. Shortest Completing Word (leetcode)
+class Solution:
+    def shortestCompletingWord(self, licensePlate: str, words: List[str]) -> str:
+        l = []
+        for i in licensePlate:
+            if i.isalpha():
+                l.append(i.lower())
+        # print(l)
+        words.sort(key=len)
+        for i in words:
+            x = 1
+            for j in l:
+                if (j not in i) or (l.count(j) > i.count(j)):
+                    x = 0
+                    break
+
+            if x == 1:
+                return i
