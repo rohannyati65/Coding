c193fbb6f89ff0fa3f0b5476a63b631c60810dda
Rohan <rohannyati65@gmail.com>
2021-11-06 11:16:36 +0530

    Day 42

--- 100 Days of Code/Day 42/2.py
+++ 100 Days of Code/Day 42/2.py
@@ -1,1 +1,8 @@
-#  (leetcode)
+# 260. Single Number III (leetcode)
+class Solution:
+    def singleNumber(self, nums: List[int]) -> List[int]:
+        l = []
+        for i in nums:
+            if nums.count(i) == 1:
+                l.append(i)
+        return l
