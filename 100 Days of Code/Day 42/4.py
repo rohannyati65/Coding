ac5a3d6befc9ef2ab627f24b6b6512a5a8135622
Rohan <rohannyati65@gmail.com>
2021-11-07 09:21:01 +0530

    Day 42

--- 100 Days of Code/Day 42/4.py
+++ 100 Days of Code/Day 42/4.py
@@ -1,1 +1,10 @@
-#  (leetcode)
+# 66. Plus One (leetcode)
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        x = ""
+        for i in digits:
+            x = x + str(i)
+
+        x = str(int(x) + 1)
+        l = [int(i) for i in x]
+        return l
