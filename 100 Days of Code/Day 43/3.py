ca720ca4aab64a1b5832a53d5e9523fc6a3eee7e
Rohan <rohannyati65@gmail.com>
2021-11-07 09:34:40 +0530

    Day 43

--- 100 Days of Code/Day 43/3.py
+++ 100 Days of Code/Day 43/3.py
@@ -1,1 +1,10 @@
-#  (leetcode)
+# 989. Add to Array-Form of Integer (leetcode)
+class Solution:
+    def addToArrayForm(self, num: List[int], k: int) -> List[int]:
+        x = ""
+        for i in num:
+            x = x + str(i)
+
+        x = str(int(x) + k)
+        l = [int(i) for i in x]
+        return l
