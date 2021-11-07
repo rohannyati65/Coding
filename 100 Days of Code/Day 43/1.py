87ad337dc2d49ae4472ee898882aa3455bfac9cd
Rohan <rohannyati65@gmail.com>
2021-11-07 09:29:32 +0530

    Day 43

--- 100 Days of Code/Day 43/1.py
+++ 100 Days of Code/Day 43/1.py
@@ -1,1 +1,5 @@
-#  (leetcode)
+# 67. Add Binary (leetcode)
+class Solution:
+    def addBinary(self, a: str, b: str) -> str:
+        n = int(a, 2) + int(b, 2)
+        return bin(n).replace("0b", "")
