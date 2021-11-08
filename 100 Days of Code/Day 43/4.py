04d8c675410a74584fa5d1ec59986683e799102a
Rohan <rohannyati65@gmail.com>
2021-11-08 13:11:38 +0530

    Day 43

--- 100 Days of Code/Day 43/4.py
+++ 100 Days of Code/Day 43/4.py
@@ -1,1 +1,7 @@
-#  (leetcode)
+# 1002. Find Common Characters (leetcode)
+class Solution:
+    def commonChars(self, words: List[str]) -> List[str]:
+        res = collections.Counter(words[0])
+        for a in words:
+            res &= collections.Counter(a)
+        return list(res.elements())
