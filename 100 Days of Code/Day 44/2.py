9199110757d030bb613256b79143419805b0011b
Rohan <rohannyati65@gmail.com>
2021-11-11 11:23:10 +0530

    Day 44

--- 100 Days of Code/Day 44/2.py
+++ 100 Days of Code/Day 44/2.py
@@ -1,1 +1,18 @@
-#  (leetcode)
+# 1413. Minimum Value to Get Positive Step by Step Sum (leetcode)
+class Solution:
+    def minStartValue(self, nums: List[int]) -> int:
+        a = 1
+        x = False
+        while x != True:
+            # print(a)
+            y = a
+            x = True
+            for i in nums:
+                y = y + i
+                if y < 1:
+                    x = False
+                    break
+
+            if x == False:
+                a = a + 1
+        return a
