d5dae52d16fea2c72075a9cc2991d7e00c63653e
Rohan <rohannyati65@gmail.com>
2021-11-11 11:07:43 +0530

    Day 44

--- 100 Days of Code/Day 44/1.py
+++ 100 Days of Code/Day 44/1.py
@@ -1,2 +1,16 @@
-# 38. Count and Say (leetcode)
-# here we go
+# 2062. Count Vowel Substrings of a String (leetcode)
+class Solution:
+    def countVowelSubstrings(self, word: str) -> int:
+        l = ["a", "e", "i", "o", "u"]
+        x = 0
+        for i in range(len(word) - 4):
+            for j in range(i + 5, len(word) + 1):
+                y = word[i:j]
+                l1 = [z for z in y]
+                s1 = list(set(l1))
+                s1.sort()
+                # print(y)
+                if s1 == l:
+                    # print(y)
+                    x = x + 1
+        return x
