304d843222a504d608208c1fa7825f2901aee04a
Rohan <rohannyati65@gmail.com>
2021-11-11 13:33:42 +0530

    Day 44

--- 100 Days of Code/Day 44/4.py
+++ 100 Days of Code/Day 44/4.py
@@ -1,1 +1,15 @@
-#  (leetcode)
+# 125. Valid Palindrome (leetcode)
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        l = ""
+        for i in s:
+            if i.isalpha():
+                l = l + i.lower()
+
+            if i.isnumeric():
+                l = l + i
+        # print(l)
+        if l == l[::-1]:
+            return True
+
+        return False
