0430322e518f53e1ed769d94a310a3d543e33cf5
Rohan <rohannyati65@gmail.com>
2021-11-12 07:43:33 +0530

    Day 44

--- 100 Days of Code/Day 45/1.py
+++ 100 Days of Code/Day 45/1.py
@@ -1,1 +1,16 @@
-#  (leetcode)
+# HackerEarth
+t = int(input())
+while t != 0:
+    n = int(input())
+    l = [int(i) for i in str(n)]
+    l.sort()
+    a = ""
+    b = ""
+    for i in range(len(l)):
+        if i % 2 == 0:
+            a = a + str(l[i])
+        else:
+            b = b + str(l[i])
+    c = int(a) + int(b)
+    print(c)
+    t = t - 1
