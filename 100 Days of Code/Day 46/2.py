7c498ba97ec7ba0afc14e2f15168abb109015a5b
Rohan <rohannyati65@gmail.com>
2021-11-16 20:34:28 +0530

    Day 46

--- 100 Days of Code/Day 46/2.py
+++ 100 Days of Code/Day 46/2.py
@@ -1,1 +1,15 @@
-#  (leetcode)
+# 2073. Time Needed to Buy Tickets (leetcode)
+class Solution:
+    def timeRequiredToBuy(self, tickets: List[int], k: int) -> int:
+        a = tickets[k]
+        x = 0
+        while a != 0:
+            for i in range(len(tickets)):
+                if tickets[i] != 0:
+                    x = x + 1
+                    tickets[i] = tickets[i] - 1
+                a = tickets[k]
+                if a == 0:
+                    return x
+            a = tickets[k]
+        return x
