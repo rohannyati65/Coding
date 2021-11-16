36118f822bdf7a516189419531f651cbccec8833
Rohan <rohannyati65@gmail.com>
2021-11-16 19:58:32 +0530

    Day 46

--- 100 Days of Code/Day 46/1.py
+++ 100 Days of Code/Day 46/1.py
@@ -1,1 +1,16 @@
-#  (leetcode)
+# 2068. Check Whether Two Strings are Almost Equivalent (leetcode)
+class Solution:
+    def checkAlmostEquivalent(self, word1: str, word2: str) -> bool:
+        for i in range(len(word1)):
+            a = word1.count(word1[i])
+            b = word2.count(word1[i])
+            if abs(a - b) > 3:
+                return False
+
+        for i in range(len(word2)):
+            a = word2.count(word2[i])
+            b = word1.count(word2[i])
+            if abs(a - b) > 3:
+                return False
+
+        return True
