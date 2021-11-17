929554a8c1b5dbad0cc7cc185c3e3466fb0142c6
Rohan <rohannyati65@gmail.com>
2021-11-17 21:02:11 +0530

    Day 47

--- 100 Days of Code/Day 47/3.py
+++ 100 Days of Code/Day 47/3.py
@@ -1,1 +1,23 @@
-#  (leetcode)
+# Problem: NEWPIECE Contest: START17C
+t = int(input())
+while t != 0:
+    x, y, a, b = map(int, input().split())
+    if x == a and y == b:
+        print(0)
+
+    else:
+        s = 0
+        if (x + y) % 2 == 0:
+            s = 1
+
+        f = 0
+        if (a + b) % 2 == 0:
+            f = 1
+
+        if f == s:
+            print(2)
+
+        else:
+            print(1)
+
+    t = t - 1
