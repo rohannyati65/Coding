3a7c913a5f1fb0c75fcf6cebc56467f2325caa0e
Rohan <rohannyati65@gmail.com>
2021-11-17 21:00:50 +0530

    Day 47

--- 100 Days of Code/Day 47/2.py
+++ 100 Days of Code/Day 47/2.py
@@ -1,1 +1,11 @@
-#  (leetcode)
+#  Contest Code:START17C
+# Problem Code:CHEFVACATION
+
+t = int(input())
+while t != 0:
+    x, y, z = map(int, input().split())
+    if x + y <= z:
+        print("YES")
+    else:
+        print("NO")
+    t = t - 1
