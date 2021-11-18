720e7b1505a9aae8ff5020ea80fd4c01bfb8007f
Rohan <rohannyati65@gmail.com>
2021-11-18 22:16:08 +0530

    Day 48

--- 100 Days of Code/Day 48/3.py
+++ 100 Days of Code/Day 48/3.py
@@ -1,1 +1,42 @@
-#  (leetcode)
+# 806. Number of Lines To Write String (leetcode)
+class Solution:
+    def numberOfLines(self, widths: List[int], s: str) -> List[int]:
+        l = [
+            "a",
+            "b",
+            "c",
+            "d",
+            "e",
+            "f",
+            "g",
+            "h",
+            "i",
+            "j",
+            "k",
+            "l",
+            "m",
+            "n",
+            "o",
+            "p",
+            "q",
+            "r",
+            "s",
+            "t",
+            "u",
+            "v",
+            "w",
+            "x",
+            "y",
+            "z",
+        ]
+
+        x = 1
+        s1 = 0
+        for i in s:
+            a = l.index(i)
+            if s1 + widths[a] > 100:
+                s1 = widths[a]
+                x += 1
+            else:
+                s1 = s1 + widths[a]
+        return [x, s1]
