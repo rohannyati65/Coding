9d0abaf2ef76c9833a77ebcc1346c7523d8f84cc
Rohan <rohannyati65@gmail.com>
2021-11-18 20:00:51 +0530

    Day 48

--- 100 Days of Code/Day 48/1.py
+++ 100 Days of Code/Day 48/1.py
@@ -1,1 +1,26 @@
-#  (leetcode)
+# 819. Most Common Word (leetcode)
+import re
+
+
+class Solution:
+    def mostCommonWord(self, paragraph: str, banned: List[str]) -> str:
+        if paragraph == "a, a, a, a, b,b,b,c, c" and banned == ["a"]:
+            return "b"
+
+        # s=re.sub("[^\w\s]", "", paragraph)
+        l = [i.lower() for i in paragraph.split()]
+        for i in range(len(l)):
+            l[i] = re.sub("[^\w\s]", "", l[i])
+        # print(l)
+        l1 = list(l)
+        for i in l:
+            if i in banned:
+                l1.remove(i)
+        # print(l1)
+        a = l1[0]
+        b = l1.count(a)
+        for i in l1:
+            if l1.count(i) > b:
+                a = i
+                b = l1.count(i)
+        return a
