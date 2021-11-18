50811f49cef9a7db902bf6efc8ff629a39ba6140
Rohan <rohannyati65@gmail.com>
2021-11-18 09:18:29 +0530

    Day 47

--- 100 Days of Code/Day 47/4.py
+++ 100 Days of Code/Day 47/4.py
@@ -1,1 +1,22 @@
-#  (leetcode)
+# 448. Find All Numbers Disappeared in an Array (leetcode)
+class Solution:
+    def findDisappearedNumbers(self, nums: List[int]) -> List[int]:
+        set1 = set(nums)
+        if len(nums) == len(set1):
+            x = max(nums)
+            lst = []
+            for i in range(1, x):
+                lst.append(i)
+            set2 = set(lst)
+            set3 = set2.difference(set1)
+            lst1 = list(set3)
+            return lst1
+        else:
+            z = len(nums)
+            lst = []
+            for i in range(1, z + 1):
+                lst.append(i)
+            set2 = set(lst)
+            set3 = set2.difference(set1)
+            lst1 = list(set3)
+            return lst1
