2fdf46c0684841671ad187f3c4b5a272456b0276
Rohan <rohannyati65@gmail.com>
2021-11-19 11:28:04 +0530

    Day 49

--- 100 Days of Code/Day 49/2.py
+++ 100 Days of Code/Day 49/2.py
@@ -1,1 +1,17 @@
-#  (leetcode)
+# 1160. Find Words That Can Be Formed by Characters (leetcode)
+class Solution:
+    def countCharacters(self, words: List[str], chars: str) -> int:
+        count = 0
+        for i in words:
+            if len(i) <= len(chars):
+                l = [j for j in i]
+                l1 = list(set(l))
+                x = True
+                for k in l1:
+                    if (k not in chars) or (l.count(k) > chars.count(k)):
+                        x = False
+                        break
+
+                if x == True:
+                    count += len(i)
+        return count
