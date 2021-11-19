b794022c0f8a7d7000efd39be6e8b2c5815c2a81
Rohan <rohannyati65@gmail.com>
2021-11-19 12:10:32 +0530

    Day 49

--- 100 Days of Code/Day 49/3.py
+++ 100 Days of Code/Day 49/3.py
@@ -1,14 +1,17 @@
 # 1047. Remove All Adjacent Duplicates In String (leetcode)
+# In this code we compare the current value with the previous value .
+# For better understanding remove the "#" commented part .
 class Solution:
     def removeDuplicates(self, s: str) -> str:
-        first=1
-        last=len(s)
-        while(first<last):
-            if(s[first]==s[first-1]):
-                s=s.replace(s[first-1:first+1],"")
-                if(first>1):
-                    first=first-1
+        first = 1
+        last = len(s)
+        while first < last:
+            if s[first] == s[first - 1]:
+                s = s.replace(s[first - 1 : first + 1], "")
+                if first > 1:
+                    first = first - 1
             else:
-                first+=1
-            last=len(s)
-        return(s)
+                first += 1
+            last = len(s)
+            # print(s,first,last)
+        return s
