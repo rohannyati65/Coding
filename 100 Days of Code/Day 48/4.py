02f8676d33e469f50d05b838728bb25f5711caa8
Rohan <rohannyati65@gmail.com>
2021-11-19 10:07:58 +0530

    Day 48

--- 100 Days of Code/Day 48/4.py
+++ 100 Days of Code/Day 48/4.py
@@ -1,1 +1,17 @@
-#  (leetcode)
+# 461. Hamming Distance (leetcode)
+class Solution:
+    def hammingDistance(self, x: int, y: int) -> int:
+        if x > y:
+            x, y = y, x
+        a = str(bin(x))
+        a = a[2:]
+        b = str(bin(y))
+        b = b[2:]
+        # print(a,b)
+        a = "0" * (len(b) - len(a)) + a
+        # print(a,b)
+        count = 0
+        for i in range(len(a)):
+            if a[i] != b[i]:
+                count += 1
+        return count
