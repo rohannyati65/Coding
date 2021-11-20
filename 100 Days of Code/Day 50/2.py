bb01197e7b1eccdd8e7e73d424f3b5f5f759eabe
Rohan <rohannyati65@gmail.com>
2021-11-20 21:55:53 +0530

    Day 50

--- 100 Days of Code/Day 50/2.py
+++ 100 Days of Code/Day 50/2.py
@@ -1,1 +1,14 @@
-#  (leetcode)
+# 1935. Maximum Number of Words You Can Type (leetcode)
+class Solution:
+    def canBeTypedWords(self, text: str, brokenLetters: str) -> int:
+        l = list(map(str, text.split()))
+        count = 0
+        for i in l:
+            flag = True
+            for j in i:
+                if j in brokenLetters:
+                    flag = False
+                    break
+            if flag == True:
+                count += 1
+        return count
