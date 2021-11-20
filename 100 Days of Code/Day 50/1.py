0aec627818edf7ae0ecc9d39404364abec6d4aaa
Rohan <rohannyati65@gmail.com>
2021-11-20 19:02:44 +0530

    Day 50

--- 100 Days of Code/Day 50/1.py
+++ 100 Days of Code/Day 50/1.py
@@ -1,1 +1,23 @@
-#  (leetcode)
+# 1071. Greatest Common Divisor of Strings (leetcode)
+class Solution:
+    def gcdOfStrings(self, str1: str, str2: str) -> str:
+        if len(str2) > len(str1):
+            str1, str2 = str2, str1
+
+        l = []
+        for i in range(len(str2)):
+            for j in range(i + 1, len(str2) + 1):
+                a = str2[i:j]
+                l.append(a)
+        l.sort(key=len)
+        l = l[::-1]
+        # print(l)
+        x = ""
+        for i in l:
+            if i in str1:
+                a = str1.count(i)
+                b = str2.count(i)
+                if len(str1) == (len(i) * a) and (len(str2) == (len(i) * b)):
+                    x = i
+                    break
+        return x
