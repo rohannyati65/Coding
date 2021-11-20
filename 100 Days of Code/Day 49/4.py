cf9d2bbce81953ea26fd60665635bb2d6366c320
Rohan <rohannyati65@gmail.com>
2021-11-20 11:22:48 +0530

    Day 49

--- 100 Days of Code/Day 49/4.py
+++ 100 Days of Code/Day 49/4.py
@@ -1,1 +1,6 @@
-#  (leetcode)
+# 540. Single Element in a Sorted Array (leetcode)
+class Solution:
+    def singleNonDuplicate(self, nums: List[int]) -> int:
+        for i in nums:
+            if nums.count(i) == 1:
+                return i
