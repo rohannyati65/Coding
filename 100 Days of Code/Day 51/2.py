de69bfe65907ea09530ddd8f57256c279af90edd
Rohan <rohannyati65@gmail.com>
2021-11-21 18:47:08 +0530

    51

--- 100 Days of Code/Day 51/2.py
+++ 100 Days of Code/Day 51/2.py
@@ -1,1 +1,15 @@
-#  (leetcode)
+# 2079. Watering Plants (leetcode)
+class Solution:
+    def wateringPlants(self, plants: List[int], capacity: int) -> int:
+        x = capacity
+        count = 0
+        for i in range(len(plants)):
+            if x < plants[i]:
+                count = count + i + i + 1
+                x = capacity - plants[i]
+
+            else:
+                count = count + 1
+                x = x - plants[i]
+
+        return count
