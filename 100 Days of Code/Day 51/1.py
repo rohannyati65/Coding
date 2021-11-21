07ec4cf6cbc190706b77f390725683c0b1df160e
Rohan <rohannyati65@gmail.com>
2021-11-21 18:22:21 +0530

    51

--- 100 Days of Code/Day 51/1.py
+++ 100 Days of Code/Day 51/1.py
@@ -1,1 +1,10 @@
-#  (leetcode)
+# Evaluate Expression (Coding Ninjas)
+n = int(input())
+x = 0
+for i in range(n):
+    a = int(input())
+    a = str(a)
+    y = int(a[:-1])
+    z = int(a[-1])
+    x = x + y ** z
+print(x)
