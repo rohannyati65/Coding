f2fa4476187769099f7cbb37d1db9ff6cff5bbde
Rohan <rohannyati65@gmail.com>
2021-11-21 13:12:09 +0530

    Day 50

--- 100 Days of Code/Day 50/4.py
+++ 100 Days of Code/Day 50/4.py
@@ -1,1 +1,19 @@
-#  (leetcode)
+# 2078. Two Furthest Houses With Different Colors (leetcode)
+class Solution:
+    def maxDistance(self, colors: List[int]) -> int:
+        x = 0
+        first = 0
+        last = len(colors) - 1
+        while first < last:
+            if colors[first] != colors[last]:
+                x = max(x, last - first)
+            last = last - 1
+
+        first = 0
+        last = len(colors) - 1
+        while first < last:
+            if colors[first] != colors[last]:
+                x = max(x, last - first)
+            first += 1
+
+        return x
