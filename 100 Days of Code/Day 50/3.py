557bb9f5050b4d513225f2d1cad2de5e4aad79a0
Rohan <rohannyati65@gmail.com>
2021-11-21 10:57:26 +0530

    Day 50

--- 100 Days of Code/Day 50/3.py
+++ 100 Days of Code/Day 50/3.py
@@ -1,1 +1,14 @@
-#  (leetcode)
+# 13. Roman to Integer (leetcode)
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        l = [1, 5, 10, 50, 100, 500, 1000]
+        y = s[::-1]
+        lst = ["I", "V", "X", "L", "C", "D", "M"]
+        count = l[lst.index(y[0])]
+        for i in range(1, len(y)):
+            if l[lst.index(y[i])] >= l[lst.index(y[i - 1])]:
+                count += l[lst.index(y[i])]
+
+            else:
+                count = count - l[lst.index(y[i])]
+        return count
