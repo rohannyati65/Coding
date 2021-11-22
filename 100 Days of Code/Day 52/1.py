e6f0353064bccbcee0b2ad1d0050a18269e764f5
Rohan <rohannyati65@gmail.com>
2021-11-22 21:25:22 +0530

    52

--- 100 Days of Code/Day 52/1.py
+++ 100 Days of Code/Day 52/1.py
@@ -1,1 +1,12 @@
-#  (leetcode)
+# Problem: MAKEEQUAL Contest: START18C
+t = int(input())
+while t != 0:
+    n = int(input())
+    l = list(map(int, input().split()))
+    if l.count(l[0]) == len(l):
+        print(0)
+    else:
+        x = max(l)
+        y = min(l)
+        print(x - y)
+    t = t - 1
