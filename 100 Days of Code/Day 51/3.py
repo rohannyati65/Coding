cfb51a59ae5964067738d9df05d904639be2bae5
Rohan <rohannyati65@gmail.com>
2021-11-22 18:17:58 +0530

    51

--- 100 Days of Code/Day 51/3.py
+++ 100 Days of Code/Day 51/3.py
@@ -1,1 +1,16 @@
-#  (leetcode)
+# 455. Assign Cookies (leetcode)
+class Solution:
+    def findContentChildren(self, g: List[int], s: List[int]) -> int:
+        g.sort()
+        s.sort()
+        x = 0
+        for i in g:
+            if len(s) == 0:
+                break
+            else:
+                for j in s:
+                    if j >= i:
+                        x = x + 1
+                        s.remove(j)
+                        break
+        return x
