667c6c178980b90b34db57d041e518346ecfc34f
Rohan <rohannyati65@gmail.com>
2021-11-22 21:23:50 +0530

    51

--- 100 Days of Code/Day 51/4.py
+++ 100 Days of Code/Day 51/4.py
@@ -1,1 +1,10 @@
-#  (leetcode)
+# Problem: MINDAYSRET Contest: START18C
+t = int(input())
+while t != 0:
+    n, k = map(int, input().split())
+    c = 0
+    while n > 0:
+        n = n - k
+        c += 1
+    print(c)
+    t = t - 1
