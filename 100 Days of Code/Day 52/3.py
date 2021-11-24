adea25aa8840f1683f13d52290de4c7d7c6da65f
Rohan <rohannyati65@gmail.com>
2021-11-24 19:33:44 +0530

    52

--- 100 Days of Code/Day 52/3.py
+++ 100 Days of Code/Day 52/3.py
@@ -1,1 +1,23 @@
-#  (leetcode)
+# 986. Interval List Intersections (leetcode)
+class Solution:
+    def intervalIntersection(
+        self, firstList: List[List[int]], secondList: List[List[int]]
+    ) -> List[List[int]]:
+        l = []
+        if len(firstList) != 0:
+            for i in firstList:
+                if len(secondList) != 0:
+                    for j in secondList:
+                        if i[0] == j[1]:
+                            if [i[0], i[0]] not in l:
+                                l.append([i[0], i[0]])
+                        elif i[1] == j[0]:
+                            if [i[1], i[1]] not in l:
+                                l.append([i[1], i[1]])
+                        else:
+                            if (i[0] in range(j[0], j[1] + 1)) or (
+                                j[0] in range(i[0], i[1] + 1)
+                            ):
+                                if [max(i[0], j[0]), min(i[1], j[1])] not in l:
+                                    l.append([max(i[0], j[0]), min(i[1], j[1])])
+        return l
