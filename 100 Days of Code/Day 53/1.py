eac8957d9d958ef41a275a1d2694c9dee5bbfa7b
Rohan <rohannyati65@gmail.com>
2021-11-25 21:04:39 +0530

    Day 53

--- 100 Days of Code/Day 53/1.py
+++ 100 Days of Code/Day 53/1.py
@@ -1,1 +1,24 @@
-#  (leetcode)
+# Codeforces Round #756 (Div. 3), problem: (A) Make Even, Accepted
+t = int(input())
+while t != 0:
+    n = int(input())
+    n = str(n)
+    if int(n[-1]) % 2 == 0:
+        print(0)
+
+    elif int(n[0]) % 2 == 0:
+        print(1)
+
+    else:
+        x = 0
+        for i in range(len(n)):
+            if int(n[i]) % 2 == 0:
+                x = x + 1
+                break
+
+        if x == 0:
+            print(-1)
+        else:
+            print(x + 1)
+
+    t = t - 1
