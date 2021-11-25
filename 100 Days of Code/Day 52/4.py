5f8c79577108d7c63edb102d01409f40ee14eed1
Rohan <rohannyati65@gmail.com>
2021-11-25 18:17:29 +0530

    52

--- 100 Days of Code/Day 52/4.py
+++ 100 Days of Code/Day 52/4.py
@@ -1,1 +1,17 @@
-#  (leetcode)
+# 53. Maximum Subarray (leetcode)
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        if len(nums) == 1:
+            return nums[0]
+
+        max_tn = -12345
+        max_e = 0
+
+        for i in nums:
+            max_e = max_e + i
+
+            max_tn = max(max_tn, max_e)
+
+            if max_e < 0:
+                max_e = 0
+        return max_tn
