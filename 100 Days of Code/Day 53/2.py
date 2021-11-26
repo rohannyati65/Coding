cb39d92ad432c85318de38dc3a9258c5cc5a5197
Rohan <rohannyati65@gmail.com>
2021-11-26 10:43:07 +0530

    Day 53

--- 100 Days of Code/Day 53/2.py
+++ 100 Days of Code/Day 53/2.py
@@ -1,1 +1,11 @@
-#  (leetcode)
+# 35. Search Insert Position (leetcode)
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        if target in nums:
+            x = nums.index(target)
+            return x
+        else:
+            nums.append(target)
+            nums.sort()
+            x = nums.index(target)
+            return x
