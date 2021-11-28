96dbba0e2a022cbb31382f73d37ccff9410c77c6
Rohan <rohannyati65@gmail.com>
2021-11-28 11:15:31 +0530

    4 codes a day

--- 100 Days of Code/Day 54/3.py
+++ 100 Days of Code/Day 54/3.py
@@ -1,1 +1,9 @@
-#  (leetcode)
+# 2089. Find Target Indices After Sorting Array (leetcode)
+class Solution:
+    def targetIndices(self, nums: List[int], target: int) -> List[int]:
+        nums.sort()
+        l = []
+        for i in range(len(nums)):
+            if nums[i] == target:
+                l.append(i)
+        return l
