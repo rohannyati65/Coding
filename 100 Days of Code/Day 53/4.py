e829cd502faee2d806ba048723151f46a768dc8d
Rohan <rohannyati65@gmail.com>
2021-11-28 10:54:08 +0530

    4 codes a day

--- 100 Days of Code/Day 53/4.py
+++ 100 Days of Code/Day 53/4.py
@@ -1,1 +1,10 @@
-#  (leetcode)
+# Problem: CSTOCK Contest: LTIME102C User: rohan_nyati
+t = int(input())
+while t != 0:
+    s1, a, b, c1 = map(int, input().split())
+    x = s1 + ((s1 * c1) // 100)
+    if x in range(a, b + 1):
+        print("YES")
+    else:
+        print("NO")
+    t = t - 1
