e2c538eb8d0ab8cc37f1a6c7759393e2423cc179
Rohan <rohannyati65@gmail.com>
2021-11-28 11:12:23 +0530

    4 codes a day

--- 100 Days of Code/Day 54/2.py
+++ 100 Days of Code/Day 54/2.py
@@ -1,1 +1,10 @@
-#  (leetcode)
+# 2085. Count Common Words With One Occurrence (leetcode)
+class Solution:
+    def countWords(self, words1: List[str], words2: List[str]) -> int:
+        if len(words2) > len(words1):
+            words1, words2 = words2, words1
+        x = 0
+        for i in words1:
+            if words1.count(i) == 1 and words2.count(i) == 1:
+                x += 1
+        return x
