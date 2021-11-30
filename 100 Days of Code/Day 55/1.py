0b283fef12e105bb78064b4d4e6fc96cba174a9e
Rohan <rohannyati65@gmail.com>
2021-11-30 19:03:25 +0530

    4 codes a day

--- 100 Days of Code/Day 55/1.py
+++ 100 Days of Code/Day 55/1.py
@@ -1,1 +1,8 @@
-#  (leetcode)
+# 240. Search a 2D Matrix II (leetcode)
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        for i in matrix:
+            if target in i:
+                return(True)
+        return(False)
+        
