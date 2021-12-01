0002ea36d0a309d14d8f766867595bce73aa0d7d
Rohan <rohannyati65@gmail.com>
2021-12-01 11:35:01 +0530

    4 codes a day

--- 100 Days of Code/Day 55/2.py
+++ 100 Days of Code/Day 55/2.py
@@ -1,1 +1,9 @@
-#  (leetcode)
+# Problem: S01E01 Contest: PRACTICE(SCHOOL) User: rohan_nyati
+t = int(input())
+while t != 0:
+    n = int(input())
+    if n >= 21:
+        print("YES")
+    else:
+        print("NO")
+    t = t - 1
