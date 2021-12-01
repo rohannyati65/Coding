ae18e99ab49c9b41a711af01321e886ce3a2f961
Rohan <rohannyati65@gmail.com>
2021-12-01 11:36:09 +0530

    4 codes a day

--- 100 Days of Code/Day 55/3.py
+++ 100 Days of Code/Day 55/3.py
@@ -1,1 +1,17 @@
-#  (leetcode)
+# Problem: S02E10 Contest: PRACTICE(SCHOOL) User: rohan_nyati
+t = int(input())
+while t != 0:
+    n, x, k = map(int, input().split())
+    a = list(map(int, input().split()))
+    b = list(map(int, input().split()))
+    c = 0
+    for i in range(n):
+        if abs(a[i] - b[i]) <= k:
+            c += 1
+
+    if c >= x:
+        print("YES")
+    else:
+        print("NO")
+
+    t = t - 1
