dedf12db9ed9a6b106625c18d6eee9a166b1d316
Rohan <rohannyati65@gmail.com>
2021-12-02 12:19:14 +0530

    4 codes a day

--- 100 Days of Code/Day 55/4.py
+++ 100 Days of Code/Day 55/4.py
@@ -1,1 +1,10 @@
-#  (leetcode)
+# Contest Code:PRACTICE Problem Code:DDCW1_01
+t = int(input())
+while t != 0:
+    a, b, c, n = map(int, input().split())
+    l = [a, b, c]
+    while len(l) != n:
+        x = l[-1] + l[-2] + l[-3]
+        l.append(x)
+    print((l[-1]) % (1000000007))
+    t = t - 1
