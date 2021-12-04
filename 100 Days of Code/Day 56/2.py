5175a941c1b4580b335f38ba0ccfb84707d6b407
Rohan <rohannyati65@gmail.com>
2021-12-04 13:53:49 +0530

    4 codes a day

--- 100 Days of Code/Day 56/2.py
+++ 100 Days of Code/Day 56/2.py
@@ -1,1 +1,10 @@
-#  (leetcode)
+# Hot wheels
+v=int(input())
+w=int(input())
+if (w&1)==1 or w<2 or w<=v:
+    print("INVALID INPUT")
+else:
+    x=((4*v) -w)//2
+    print(x)
+    print(v-x)
+    
--- 100 Days of Code/Day 56/3.py
+++ 100 Days of Code/Day 56/3.py
@@ -1,1 +1,15 @@
-#  (leetcode)
+# Mike's intelligence
+t = int(input())
+while t != 0:
+    x = int(input())
+    y = 1
+    z = 0
+    for i in range(1, x + 1):
+        y = y * i
+        z = z + i
+
+    if y % z == 0:
+        print("EURIKA")
+    else:
+        print("HELL NO")
+    t = t - 1
