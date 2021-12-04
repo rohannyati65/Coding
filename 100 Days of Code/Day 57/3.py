16b598eb957632fd0c55a028ca347464665ea97e
Rohan <rohannyati65@gmail.com>
2021-12-04 14:04:50 +0530

    4 codes a day

--- 100 Days of Code/Day 57/3.py
+++ 100 Days of Code/Day 57/3.py
@@ -1,1 +1,43 @@
-#  (leetcode)
+# Paren-Magic
+def areBracketsBalanced(expr):
+    stack = []
+
+    for char in expr:
+        if char in ["(", "{", "["]:
+            stack.append(char)
+        else:
+            if not stack:
+                return False
+            current_char = stack.pop()
+            if current_char == "(":
+                if char != ")":
+                    return False
+            if current_char == "{":
+                if char != "}":
+                    return False
+            if current_char == "[":
+                if char != "]":
+                    return False
+
+    if stack:
+        return False
+    return True
+
+
+t = int(input())
+while t != 0:
+    s = input()
+    l = []
+    for i in range(len(s)):
+        for j in range(i, len(s) + 1):
+            l.append(s[i:j])
+    # print(l)
+    x = 0
+    y = ""
+    for i in l:
+        if len(i) > x:
+            if areBracketsBalanced(i):
+                x = len(i)
+                y = i
+    print(y)
+    t = t - 1
