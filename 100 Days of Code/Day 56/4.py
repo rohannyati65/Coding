d39449474eef9ba33003d01af2eb4b6616352eff
Rohan <rohannyati65@gmail.com>
2021-12-04 14:02:24 +0530

    4 codes a day

--- 100 Days of Code/Day 56/4.py
+++ 100 Days of Code/Day 56/4.py
@@ -1,1 +1,31 @@
-#  (leetcode)
+# Help the Assassin !!!
+class Solution(object):
+    def canCross(self, stones):
+        n = len(stones)
+        stoneSet = set(stones)
+        visited = set()
+
+        def goFurther(value, units):
+            if (value + units not in stoneSet) or ((value, units) in visited):
+                return False
+            if value + units == stones[n - 1]:
+                return True
+            visited.add((value, units))
+            return (
+                goFurther(value + units, units)
+                or goFurther(value + units, units - 1)
+                or goFurther(value + units, units + 1)
+            )
+
+        print(goFurther(stones[0], 1))
+
+
+s = input()
+# print(s)
+l = []
+for i in s:
+    if i.isnumeric():
+        l.append(int(i))
+# print(l)
+s = Solution()
+s.canCross(l)
--- 100 Days of Code/Day 57/1.py
+++ 100 Days of Code/Day 57/1.py
@@ -1,1 +1,18 @@
-#  (leetcode)
+# All-possibilities
+import itertools
+
+n = int(input())
+x = str(n)
+l1 = [i for i in x]
+l = list(map(list, itertools.permutations(l1, len(l1))))
+# print(l)
+l2 = []
+for i in l:
+    y = ""
+    for j in i:
+        y = y + j
+    if int(y) not in l2:
+        l2.append(int(y))
+l2.sort()
+for i in l2:
+    print(i)
