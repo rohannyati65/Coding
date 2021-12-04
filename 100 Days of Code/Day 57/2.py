230c4dd6dcd23b84be88133a0f56abff63d70a65
Rohan <rohannyati65@gmail.com>
2021-12-04 14:03:12 +0530

    4 codes a day

--- 100 Days of Code/Day 57/2.py
+++ 100 Days of Code/Day 57/2.py
@@ -1,1 +1,21 @@
-#  (leetcode)
+# Save-the-Village
+t = int(input())
+while t != 0:
+    n, z = map(int, input().split())
+    l = list(map(int, input().split()))
+    l.sort()
+    count = 0
+    a = 1
+    while z > 0:
+        z = z - l[-1]
+        l[-1] = l[-1] // 2
+        l.sort()
+        count += 1
+        if l[-1] == 0:
+            a = 0
+            break
+    if a == 1:
+        print(count)
+    if a == 0:
+        print("Run {}".format(z))
+    t = t - 1
