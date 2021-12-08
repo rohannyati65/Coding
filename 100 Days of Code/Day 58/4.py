f4c7b67dd729b7cfd8f94cfdd6767b4752a60bdd
Rohan <rohannyati65@gmail.com>
2021-12-08 22:03:38 +0530

    4 codes a day

--- 100 Days of Code/Day 58/4.py
+++ 100 Days of Code/Day 58/4.py
@@ -1,1 +1,18 @@
-#  (leetcode)
+# 5. Longest Palindromic Substring (leetcode)
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        if len(s) == 1:
+            return s
+        if s == s[::-1]:
+            return s
+        else:
+            c = s[0]
+            for i in range(len(s)):
+                x = ""
+                for j in s[i:]:
+                    x = x + j
+                    y = x[::-1]
+                    if x == y:
+                        if len(x) > len(c):
+                            c = x
+            return c
