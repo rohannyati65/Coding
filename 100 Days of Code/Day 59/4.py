ee99f11cba3052ac1e2e6dd52a049ac69e6e61cb
Rohan <rohannyati65@gmail.com>
2022-01-26 17:03:40 +0530

    4 codes a day

--- 100 Days of Code/Day 59/4.py
+++ 100 Days of Code/Day 59/4.py
@@ -1,1 +1,28 @@
-#  (leetcode)
+# 121. Best Time to Buy and Sell Stock (leetcode)
+# 1st Method (TLE)
+
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        a = 0
+        for i in range(len(prices) - 1):
+            x = prices[i]
+            y = max(prices[i:])
+            if y - x > a:
+                a = y - x
+        return a
+
+
+# 2nd Methode (ACCEPTED)
+
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        p = 0
+        y = max(prices)
+        for i in range(len(prices)):
+            x = prices[i]
+            y = min(y, x)
+            z = x - y
+            p = max(p, z)
+        return p
