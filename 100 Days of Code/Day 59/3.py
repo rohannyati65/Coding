7c9bdcedcc6517fac6cf109809f18182308738bc
Rohan <rohannyati65@gmail.com>
2022-01-26 16:02:27 +0530

    4 codes a day

--- 100 Days of Code/Day 59/3.py
+++ 100 Days of Code/Day 59/3.py
@@ -1,1 +1,17 @@
-#  (leetcode)
+# Two Sum (leetcode)
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        for i in range(len(nums)):
+            if (
+                (nums[i] - target in nums)
+                and (i != nums.index(nums[i] - target))
+                and (nums[i] + nums[nums.index(nums[i] - target)] == target)
+            ):
+                return [i, nums.index(nums[i] - target)]
+
+            elif (
+                (target - nums[i]) in nums
+                and (i != nums.index(target - nums[i]))
+                and (nums[i] + nums[nums.index(target - nums[i])] == target)
+            ):
+                return [i, nums.index(target - nums[i])]
